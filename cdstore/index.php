feec88478314f84cbfa4db2adb19424981df913e
emartinez888 <emartinez888@gmail.com>
2012-08-17 11:23:41 -0400

    more updating

--- cdstore/index.php
+++ cdstore/index.php
@@ -11,25 +11,35 @@
     
 
 ?>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<link rel="stylesheet" type="text/css" href="main.css" />
+	<title>My CD list</title>
 <body>
-<form action="index.php" method="post">
-<h1>Top Selling Albums of all Time</h1><br/>
-<ul>
-	<?php echo $cdlist; ?>
-</ul>
-<br/>
-<label for="cdname">Add a CD name:</label><br />
-<input type="text" name="cdname"/><br/>
-<input type="submit" name="add" value="Add to list" />
-<br/>
-<label for="cdnumber">Delete a DC:</label><br />
-<select name="cdnumber">
-	<?php echo $cdNums; ?>
-</select>
-<br/>
-<input type="submit" name="delete" value="Delete CD from list" />
-
-</form>
+<div id="container">
+	<form action="index.php" method="post">
+		<h1>Top Selling Albums of all Time</h1><br/>
+		<ul>
+			<?php echo $cdlist; ?>
+		</ul>
+		<br/>
+		<label for="cdname">Add a CD name:</label><br />
+		<input type="text" name="cdname"/>
+		<input type="submit" name="add" value="Add to list" />
+		<br/>
+		<label for="cdnumber">Delete a DC:</label><br />
+		<select name="cdnumber">
+			<?php echo $cdNums; ?>
+		</select>
+		<input type="submit" name="delete" value="Delete CD from list" /><br/>
+		<label for="cdedit">Edit a CD name:</label><br />
+		<select name="cdnumber">
+			<?php echo $cdNums; ?>
+		</select>
+		<input type="text" name="cdedit"/>
+		<input type="submit" name="edit" value="Edit CD name" /><br/>
+	</form>
+</div>
 </body>
 </html>
--- cdstore/model/editcd.php
+++ cdstore/model/editcd.php
@@ -18,18 +18,39 @@ if(isset($_POST['delete']) && strlen($trgnum)>0){
 	$c=0;
 	$trgnum--;
 	
-	$query='SELECT * FROM cds2';
+	$query='SELECT id FROM cds2';
 	$allcds=$db->query($query);
 	
 	foreach($allcds as $theid){
 		if($c==$trgnum){
 			$target=$theid['id'];
+			$query="DELETE FROM cds2 WHERE id= $target";
+			$db->exec($query);			
 			break;
 		}
 		$c++;
 	}
-	
-	$query="DELETE FROM cds2 WHERE id= $target";
-	$db->exec($query);
+
+}
+
+$trgnum=$_POST['cdnumber'];
+$trgtxt=trim($_POST['cdedit']);
+
+if(isset($_POST['edit']) && strlen($trgnum)>0 && strlen($trgtxt)>0){
+	$c=0;
+	$trgnum--;
+
+	$query='SELECT id FROM cds2';
+	$allcds=$db->query($query);
+
+	foreach($allcds as $theid){
+		if($c==$trgnum){
+			$target=$theid['id'];
+			$query="update cds2 set name=$trgtxt WHERE id= $target";
+			$db->exec($query);			
+			break;
+		}
+		$c++;
+	}
 }
 ?>
