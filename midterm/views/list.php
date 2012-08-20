9a879a08a9c42e3117fe06c54037c60f6ec083f0
emartinez888 <emartinez888@gmail.com>
2012-08-20 10:18:30 -0400

    part 5 - the form

--- midterm/views/list.php
+++ midterm/views/list.php
@@ -8,3 +8,20 @@ foreach($days as $day){
 }
 
 ?>
+<html>
+	<body>
+		<form action='./index.php' method="post">
+			<table>
+				<tr>
+				 <th><label for="date">Enter a date:</label></th>
+				 <th><label for="members">Number of members:</label></th>
+				</tr>
+				<tr>
+				 <th><input type="text" name="date" /></th>
+				 <th><input type="text" name="members" /></th>
+				 <th><input type="submit" name="submit" value="submit" /></th>
+				</tr>
+			</table>
+		</form>
+	</body>
+</html>
